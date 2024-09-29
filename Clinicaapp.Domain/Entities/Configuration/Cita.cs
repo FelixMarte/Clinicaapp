e2c0d6fd932501b9d0d405ace6a5c747ea524c7c
FelixMarte <fjmartebalbuena@gmail.com>
2024-09-28 21:32:06 -0400

    Update Cita.cs

--- Clinicaapp.Domain/Entities/Configuration/Cita.cs
+++ Clinicaapp.Domain/Entities/Configuration/Cita.cs
@@ -1,10 +1,3 @@
-﻿using Clinicaapp.Domain.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Clinicaapp.Domain.Entities.Configuration
 {
     public class Cita : BaseEntity
