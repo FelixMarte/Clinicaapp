1fe874dcca87bf5d4002488a9983051d455ca0a7
JuanJoseFamilia <juanfamilia0411@gmail.com>
2024-10-19 23:07:29 -0400

    Update PatientsModel.cs

--- Clinicaapp.Persistence/Models/Configuracion/PatientsModel.cs
+++ Clinicaapp.Persistence/Models/Configuracion/PatientsModel.cs
@@ -1,9 +1,5 @@
 ﻿using Clinicaapp.Domain.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Clinicaapp.Persistence.Models.Configuracion
 {
