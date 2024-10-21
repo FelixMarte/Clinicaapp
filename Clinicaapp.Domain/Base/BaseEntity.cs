07177ca8d2e97e7eb137e454757bf77e07625cb4
JuanJoseFamilia <juanfamilia0411@gmail.com>
2024-10-21 15:57:00 -0400

    Update BaseEntity.cs

--- Clinicaapp.Domain/Base/BaseEntity.cs
+++ Clinicaapp.Domain/Base/BaseEntity.cs
@@ -6,7 +6,6 @@ namespace Clinicaapp.Domain.Base
         public string? LastName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public string? Password { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public bool IsActive { get; set; } = true;
